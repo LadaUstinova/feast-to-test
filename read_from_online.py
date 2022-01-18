491180591ee13d2fe82eec22e11ccbdb860cfd0a
LadaUstinova <lada.ustinova@glowbyteconsulting.com>
2022-01-18 11:14:56 +0000

    update parquet

--- read_from_online.py
+++ read_from_online.py
@@ -10,7 +10,8 @@ online_features = fs.get_online_features(
     features=features,
     entity_rows=[
         {"driver_id": 1001},
-        {"driver_id": 1002}]
+	{"driver_id": 1002},
+        {"driver_id": 1003}]
 ).to_dict()
 
 print(online_features)
