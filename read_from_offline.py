0d0c4446ab2f311980553bfd17d0640260d3b26f
AlfiyaKharlamova <alfiya.kharlamova@glowbytrconsulting.com>
2022-02-09 15:54:33 +0300

    new

---
+++ read_from_offline.py
@@ -0,0 +1,18 @@
+from feast import FeatureStore
+
+features = [
+    "driver_hourly_stats:conv_rate",
+    "driver_hourly_stats:acc_rate",
+    "driver_hourly_stats:avg_daily_trips"
+]
+
+fs = FeatureStore(repo_path="in_bug/")
+offline_features = fs.get_historical_features(
+    features=features,
+    entity_rows=[
+        {"driver_id": 1001},
+	{"driver_id": 1002},
+        {"driver_id": 1003}]
+).to_dict()
+
+print(offline_features)
